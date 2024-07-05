6b97789ef24bc36524d8cc3eb5e3c6cbfd137c56
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-07-05 14:02:15 +0000

    added datarows to display data

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function getTableHeaders(obj) {
 }
 
 function writeToDocument(type) {
+    var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
 
@@ -32,9 +33,14 @@ function writeToDocument(type) {
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-            // el.innerHTML += "<p>" + item.name + "</p>";
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow);
         });
 
-        el.innerHTML = `<table>${tableHeaders}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
